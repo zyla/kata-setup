d8eab4ace2caae9f80c88920bb0445a2791b87e5
Maciej Bielecki <zyla@prati.pl>
2025-11-18 22:07:22 +0100

    inline helm install script since github is buggy now

--- install-k3s-helm.sh
+++ install-k3s-helm.sh
@@ -3,4 +3,4 @@
 curl -sfL https://get.k3s.io | sh -s - server --flannel-backend wireguard-native --write-kubeconfig-mode=640 --write-kubeconfig-group="$(id -g)"
 mkdir -p ~/.kube
 ln -sf /etc/rancher/k3s/k3s.yaml ~/.kube/config
-curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-4 | bash
+./get-helm-4
