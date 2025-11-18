1d9fb1f66925ba36e3bd5e9c554d8fbef2ea01b9
Maciej Bielecki <zyla@prati.pl>
2025-11-18 21:26:34 +0100

    More RAM by default

--- vm-create.sh
+++ vm-create.sh
@@ -4,5 +4,5 @@ dir="$(realpath $(dirname $0))"
 
 vm_name="${1:-k3s}"
 
-limactl create --name="$vm_name" template:ubuntu-22.04 --mount-only "$dir"
+limactl create --name="$vm_name" template:ubuntu-22.04 --mount-only "$dir" --memory 16
 limactl shell --start "$vm_name" "$dir/setup.sh"
