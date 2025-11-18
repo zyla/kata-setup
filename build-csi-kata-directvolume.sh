228d11e44d65ecdf3a926464e84d34e7d6b8596d
Maciej Bielecki <zyla@prati.pl>
2025-11-18 21:47:02 +0100

    Avoid registry

--- build-csi-kata-directvolume.sh
+++ build-csi-kata-directvolume.sh
@@ -3,23 +3,6 @@
 set -euo pipefail
 cd "$(dirname $0)"
 
-# Start registry
-kubectl apply -f registry.yaml
-
-echo "Waiting for registry to be available..."
-kubectl wait --for=condition=Available deployment/registry
-echo "Registry deployment Available."
-
-nerdctl build -t localhost:5000/csi-kata-directvolume ./csi-kata-directvolume-build
-kubectl port-forward deployment/registry 5000 &
-fwd_pid="$!"
-trap 'kill $fwd_pid' EXIT
-
-for n in {1..15}; do
-  if curl -s localhost:5000; then
-    break
-  fi
-  sleep 1
-done
-
-nerdctl push localhost:5000/csi-kata-directvolume
+nerdctl build -t localhost/kata-directvolume:v1.0.19 ./csi-kata-directvolume-build
+nerdctl save localhost/kata-directvolume:v1.0.19 -o /tmp/csi-kata-directvolume.tar
+sudo k3s ctr -n k8s.io image import /tmp/csi-kata-directvolume.tar
